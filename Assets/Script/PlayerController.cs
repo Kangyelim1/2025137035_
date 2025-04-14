5bd94eca6f671dd75e8bc7bc59933b4f73a211ed
Kangyelim1 <gyelim887@gmail.com>
2025-04-14 23:32:20 +0900

    ㅇㄴㅌ

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -22,11 +23,30 @@ public class PlayerController : MonoBehaviour
         float moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
+        if (moveInput < 0)
+            transform.localScale = new Vector3(1f, 1f, 1f);
+
+        if (moveInput > 0)
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
         isGrounded = Physics2D.OverlapCircle(groumdCheck.position, 0.2f, groundLayer);
 
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      if (collision.CompareTag("Respawn"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+      if (collision.CompareTag("Finish"))
+        {
+            collision.GetComponent<LevelObject>().MoveTonextLevel();
         }
     }
 }

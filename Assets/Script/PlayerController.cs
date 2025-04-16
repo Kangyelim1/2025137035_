929333a516233112ad689a17e90bf70cd9210a63
Kangyelim1 <gyelim887@gmail.com>
2025-04-16 17:54:30 +0900

    4016

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -10,11 +10,12 @@ public class PlayerController : MonoBehaviour
     public Transform groumdCheck;
     public LayerMask groundLayer;
 
-    private Rigidbody2D rb;
-    private bool isGrounded;
-    private Animator pAni;
-
     private bool isGiant = false;
+    private bool mj = false;
+
+    private Rigidbody2D rb;
+    private Animator pAni;
+    private bool isGrounded;
 
     private void Awake()
     {
@@ -27,56 +28,59 @@ public class PlayerController : MonoBehaviour
         float moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
-        if(isGiant)
+
+        isGrounded = Physics2D.OverlapCircle(groumdCheck.position, 0.2f, groundLayer);
+
+        if (isGiant)
         {
             if (moveInput < 0)
                 transform.localScale = new Vector3(2f, 2f, 1f);
 
-            if (moveInput < 0)
+            if (moveInput > 0)
                 transform.localScale = new Vector3(-2f, 2f, 1f);
         }
-
         else
         {
             if (moveInput < 0)
                 transform.localScale = new Vector3(1f, 1f, 1f);
-
             if (moveInput > 0)
                 transform.localScale = new Vector3(-1f, 1f, 1f);
         }
 
-        isGrounded = Physics2D.OverlapCircle(groumdCheck.position, 0.2f, groundLayer);
-
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            pAni.SetTrigger("JumpAction");
         }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-      if (collision.CompareTag("Respawn"))
+        if (collision.CompareTag("Respawn"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-
-      if (collision.CompareTag("Finish"))
-        {
-            collision.GetComponent<LevelObject>().MoveTonextLevel();
-        }
-
-      if(collision.CompareTag("Enemy"))
-        {
-            if (isGiant)
-                Destroy(Collision.gameObject);
+            if(isGiant)
+                Destroy(collision.gameObject);
             else
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
 
-      if (collision.CompareTag("Item"))
+        if (collision.CompareTag("Finish"))
+        {
+            collision.GetComponent<LevelObject>().MoveTonextLevel();
+        }
+        if (collision.CompareTag("Item"))
         {
             isGiant = true;
             Destroy(collision.gameObject);
+        }
+
+       // switch (collision.tag)
+        {
+        //    case "item":
+         //       isGiant = true;
+         //       mj = true;
+         //       break;
         }
     }
 }
